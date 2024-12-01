4fe792882d61477b6d4c3643dfc181e0a95d9be7
Mohammad Hadi <hadim4@mcmaster.ca>
2024-11-30 19:55:38 -0500

    it2

--- GameMechs.h
+++ GameMechs.h
@@ -20,7 +20,7 @@ class GameMechs
         int boardSizeX;
         int boardSizeY;
 
-        objPos food;
+        objPos foodPos;
 
     public:
         GameMechs();
@@ -43,6 +43,9 @@ class GameMechs
         void incrementScore();
         
         // More methods should be added here
+
+        void generateFood(objPos blockOff);
+        objPos getFoodPos();
 };
 
 #endif
